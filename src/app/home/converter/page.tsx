94da1ad9ca411645fc915ab00db9e05ef5439480
Alcladii <122701312+Alcladii@users.noreply.github.com>
2024-09-22 09:01:41 -0700

    initial commit

--- src/app/home/converter/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Converter() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>Converter</div>
-      </main>
-    );
-  }
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-export default function Welcome() {
-  return (
-    <div style={{fontSize: '36px'}}>Welcome</div>
-  )
-}
